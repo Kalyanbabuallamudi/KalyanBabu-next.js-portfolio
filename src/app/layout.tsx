563287b293312aa55a510634115e7c585e912174
Kalyanbabuallamudi <allamudikalyanbabu56@gmail.com>
2025-09-10 17:57:53 +0530

    Update: fixed build errors, updated resume and photo, added Google Search Console file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Kalyan Babu",
   description: "Welcome to my portfolio website",
   icons: {
